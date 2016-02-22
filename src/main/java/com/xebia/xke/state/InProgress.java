f82a14fc29a3a591b4da7a8b22e20f2a40a33621
Sameer Arora <sameerarora@xebia.com>
2016-02-22 10:26:51 +0530

    Updated file formatting

--- src/main/java/com/xebia/xke/state/InProgress.java
+++ src/main/java/com/xebia/xke/state/InProgress.java
@@ -1,21 +1,20 @@
 package com.xebia.xke.state;
 //This has been added at the place where code already existed
 public class InProgress extends TaskState {
-    public InProgress(String in_progress) {
-        super(in_progress);
-    }
+  public InProgress(String in_progress) {
+    super(in_progress);
+  }
 
-    @Override
-    public void assigned(AgileTask task) {
-//sets the state to in progress
-//also adding another line to see if ranges work
-//another for the above check
-        task.setState(TaskState.IN_PROGRESS);
-    }
+  @Override
+  public void assigned(AgileTask task) {
+    //sets the state to in progress
+    //also adding another line to see if ranges work
+    //another for the above check
+    task.setState(TaskState.IN_PROGRESS);
+  }
 
-    @Override
-    public void moveToTest(AgileTask task) {
-        task.setState(TaskState.IN_TEST);
-    }
-
+  @Override
+  public void moveToTest(AgileTask task) {
+    task.setState(TaskState.IN_TEST);
+  }
 }
